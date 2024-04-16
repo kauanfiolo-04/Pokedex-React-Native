eb03ce1d6cc9ebb5e154ad91b426154f163ae8e5
tecnologia-gaslive <pedro.edu2167@gmail.com>
2024-04-16 20:27:48 -0300

    add search bar

--- frontend/components/ListPokemons.jsx
+++ frontend/components/ListPokemons.jsx
@@ -1,56 +1,72 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getPokemons } from "../loaders/getPokemons";
-import { View, StyleSheet, TextInput } from "react-native";
+import { View, StyleSheet, TextInput, FlatList } from "react-native";
 import PokeCard from "./PokeCard";
 
 const ListPokemons = ({ limit, offset }) => {
   const [pokemons, setPokemons] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     getPokemons(limit, offset).then((r) => setPokemons(r));
   }, [limit, offset]);
+
+  const filteredPokemons = pokemons.filter(pokemon =>
+    pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <PokeCard
+        id={item?.id ?? 0}
+        types={item?.types}
+        name={item?.name ?? "..."}
+        img={{
+          url: item?.sprites.front_default,
+          gif: item?.sprites.other.showdown.front_default,
+        }}
+      />
+    </View>
+  );
 
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.searchArea}
         placeholder="Type the Pokémon name"
+        value={searchQuery}
+        onChangeText={setSearchQuery}
       />
-      {pokemons.map((poke, index) => (
-        <View key={index} stytle={styles.container}>
-          <PokeCard
-            id={poke?.id ?? 0}
-            types={poke?.types}
-            name={poke?.name ?? "..."}
-            img={{
-              url: poke?.sprites.front_default,
-              gif: poke?.sprites.other.showdown.front_default,
-            }}
-          />
-        </View>
-      ))}
+      <FlatList
+        data={filteredPokemons}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        style={styles.list}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexPosition: "column",
-    width: "full",
-    justifyContent: "center",
-    gap: 8,
+    paddingVertical: 20,
+    paddingHorizontal: 7
   },
-  searchArea:{
-    width: 'full',
-    height: '5Vh',
+  searchArea: {
+    /* width: "100%",
+    height: 40,
     paddingHorizontal: 10,
-    marginVertical: 15
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5, */
+  },
+  list: {
+    /* flex: 1, */
+    width: "100%",
   },
   item: {
-    width: "full",
-    display: "flex",
-    justifyContent: "center",
+    marginVertical: 5,
   },
 });
 

07c667037a2391284f18605bf76171bf0cc38d5c
Kauan Fiolo da Silva <kauanfiolo@hotmail.com>
2024-05-03 13:58:20 -0300

    SLAA

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen'
+import PokeScreen from './screens/PokeScreen'
+
+const Stack = createNativeStackNavigator()
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="PokeApp" component={HomeScreen} />
+        <Stack.Screen name="Pokémon" component={PokeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App

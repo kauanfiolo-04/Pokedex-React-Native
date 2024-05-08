e97e3225e5be6319b01590fe4e9c10de844d4c34
tecnologia-gaslive <pedro.edu2167@gmail.com>
2024-05-08 15:06:00 -0300

    fix white space

--- frontend/components/PokeDescription.jsx
+++ frontend/components/PokeDescription.jsx
@@ -1,28 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native'
-import  getDescription  from 'loaders/getDescription'
-import { useEffect, useState } from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import getDescription from "loaders/getDescription";
+import { useEffect, useState } from "react";
 
+const PokeDescription = ({ style, pokeName }) => {
+  const [description, setDescription] = useState("");
 
+  useEffect(() => {
+    getDescription(pokeName)
+      .then((res) => {
+        const descriptionText = res.flavor_text_entries[0].flavor_text;
+        const formattedText = descriptionText.replace(/\n/g, " ");
+        setDescription(formattedText);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [pokeName]);
+  return (
+    <View style={style}>
+      <Text>{description}</Text>
+    </View>
+  );
+};
 
-const PokeDescription=({style, pokeName})=>{
-    const [description, setDesctiption] = useState('')
-    useEffect(() => {
-        getDescription(pokeName)
-        .then((res)=>{
-            setDesctiption(res.flavor_text_entries[0].flavor_text);
-            setDesctiption(description.replace(/\n/g, ' '))
-            console.log(description)
-        }).catch((err)=>{
-            console.log(err)
-        })
-    },[pokeName])
-    return(
-        <View style={style}>
-            <Text>
-                {description}
-            </Text>
-        </View>
-    )
-}
-
-export default PokeDescription
+export default PokeDescription;

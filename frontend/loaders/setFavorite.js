9876bc870b9756e72753d00937b7ecdebaea431e
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-05-10 22:03:30 -0300

    Faltando Arrumar bagulho

--- frontend/loaders/setFavorite.js
+++ frontend/loaders/setFavorite.js
@@ -1,10 +1,23 @@
 const setFavorite=async(poke)=>{
+  const {pokeId, pokeName, pokeTypes}=poke;
+  console.log(JSON.stringify({
+    pokeId, pokeName, pokeTypes
+  }))
   const url="https://pokedex-react-native.onrender.com/";
+
 
   return await fetch(url,{
     method:"post",
-    body:JSON.stringify(poke)
-  }).then(r=>r.json()).catch(error=>console.error(error));
+    body:JSON.stringify({
+      pokeId, pokeName, pokeTypes
+    })
+  }).then(r=>{
+    if(r.status===201){
+      return r.json();
+    }else{
+      return null;
+    }
+  }).catch(error=>console.error(error));
 };
 
 export default setFavorite;

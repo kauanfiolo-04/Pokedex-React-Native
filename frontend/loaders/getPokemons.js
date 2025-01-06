9344cd60c5b2b93331cf8e2fb967ea0138acbe20
kauanfiolo-04 <kauanfiolo@hotmail.com>
2025-01-06 17:22:02 -0300

    Arrumando a idetenção

--- frontend/loaders/getPokemons.js
+++ frontend/loaders/getPokemons.js
@@ -1,15 +1,15 @@
 import getPokemon from "loaders/getPokemon";
 
-export const getPokemons=async(limit, offset)=>{
-  const url=`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+export const getPokemons = async (limit, offset) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
   /* console.log('Fetching:'+ url) */
-  const pokemons=await fetch(url)
-    .then(r=>r.json()).then(data=>data.results).catch(err=>console.error(err)) ?? [];
+  const pokemons = await fetch(url)
+    .then(r => r.json()).then(data => data.results).catch(err => console.error(err)) ?? [];
 
-  const finalPokemons=[];
+  const finalPokemons = [];
 
   for(const poke of pokemons){
-    await getPokemon(poke.name).then(poke=>finalPokemons.push(poke));
+    await getPokemon(poke.name).then(poke => finalPokemons.push(poke));
   }
 
   return finalPokemons;

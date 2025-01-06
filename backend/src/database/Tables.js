f1acf6a7187f8a1a9b333459432259002f920193
kauanfiolo-04 <kauanfiolo@hotmail.com>
2025-01-06 16:51:28 -0300

    Criando table de usuário e relacionando tabela poke com ela

--- backend/src/database/Tables.js
+++ backend/src/database/Tables.js
@@ -1,16 +1,41 @@
 class Tables {
   init(connection) {
     this.connection = connection;
+    this.createTableUsers();
     this.createTablePokemons();
+  }
+
+  createTableUsers() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS users (
+        id VARCHAR(36) PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        email VARCHAR(100) NOT NULl UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+
+    this.connection.query(sql, (error) => {
+      if(error) {
+        console.log('Erro pra criar tabela pokemons');
+        console.error(error.message);
+        return;
+      }
+      console.log('Tabela users criada!');
+    });
   }
 
   createTablePokemons() {
     const sql = `
       CREATE TABLE IF NOT EXISTS pokemons (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-          pokeId VARCHAR(10),
-          pokeName VARCHAR(255),
-          pokeTypes TEXT
+        pokeId VARCHAR(10),
+        pokeName VARCHAR(255),
+        pokeTypes TEXT,
+        userId VARCHAR(36) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
       );
     `;
 
@@ -20,8 +45,8 @@ class Tables {
         console.error(error.message);
         return;
       }
-      console.log('Tabela criada');
-    })
+      console.log('Tabela pokemons criada');
+    });
   }
 }
 
